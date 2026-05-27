0c681867e2146728b547539d5a39621d76819c4b
derza-essien <d.erzaessien@outlook.com>
2026-05-27 02:11:36 +0100

    [cleanup] file changes for cpu baseline

---
+++ cpu_baseline/headers/definitions.hpp
@@ -0,0 +1,27 @@
+// relevant libs
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cmath>
+#include <thread>
+#include <unordered_map>
+
+// relevant defenitions
+
+#define ROW_NUM 720
+#define COL_NUM 1280
+#define TOTAL_LOOPS 1
+#define ITER_NUM 256
+#define NUM_THREADS 16
+
+// Enumerator for set choice
+
+#define Mandelbrot 0
+#define Julia 1
+#define Burning_Ship 2
+#define Tricorn 3
+
+
+// Set and variables for calculation
+// May not be necessary double row_start, row_end, col_start, col_end;
